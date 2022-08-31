1240e5e42801b285848d2fb5951479df0d5d5df9
RobertoValente <valeycontacto@gmail.com>
2022-08-31 19:36:57 +0100

    First Commit

---
+++ src/index.js
@@ -0,0 +1,53 @@
+/**
+ * Após clicar no ficheiro start.vbs,
+ * irá ser iniciado um Processo no seu computador
+ * (assim como se inicia-se um Programa qualquer).
+ * 
+ * Caso queira deixar de ter o RPC basta:
+ * 1. Abrir o Gestor de Tarefas 
+ * 2. Procupara por: Processos em Segundo Plano
+ * 3. Procupara por: Node.Js JavaScript Runtime
+ * 4. Clicar com Botão Direito do Mouse > Terminar Tarefa
+ */
+
+const config = require('../src/config.json')
+const discordRPC = require('discord-rpc')
+let clientId = config.rpcId;
+
+const rpc = new discordRPC.Client({ transport: 'ipc' });
+discordRPC.register(clientId);
+
+async function activity() {
+    if(!rpc) return;
+    rpc.setActivity({
+        details: config.detailsTxt,
+        state: config.stateTxt,
+        //startTimestamp: Date.now(),
+        startTimestamp: 0,
+        largeImageKey: config.largeImageKey,
+        largeImageText: config.largeImageText,
+        /*smallImageKey: config.smallImageKey,
+        smallImageText: config.smallImageText,*/
+        instance: false,
+        buttons: [
+            {
+                label: `🎁 Surpresa`,
+                url: `https://youtu.be/dQw4w9WgXcQ`,
+            },
+            {
+                label: `🤩 Crie o seu Custom-RPC`,
+                url: `https://github.com/RobertoValente`,
+            }
+        ]
+    })
+}
+
+rpc.on('ready', async () => {
+    activity();
+
+    setInterval(() => {
+        activity();
+    }, 15*1000);
+});
+
+rpc.login({ clientId }).catch(err => console.log(err));
---
+++ start.vbs
@@ -0,0 +1,2 @@
+set objShell = wscript.createobject("wscript.shell")
+    objShell.run("node src/index.js"), 0, true
